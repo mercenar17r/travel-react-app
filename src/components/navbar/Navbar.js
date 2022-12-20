40f5b370b4f5327d60e5efdb0a991b8e5f87a84b
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-20 23:22:43 +0200

    add navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h2>BEACHES.</h2>
+      </div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>Book</li>
+        <li>Views</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
