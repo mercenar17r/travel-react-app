afa93e2b7a8df25c807d4ec46f7d8e7ccca54480
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-21 23:38:30 +0200

    style navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BiSearch } from "react-icons/bi";
 import { BsPerson } from "react-icons/bs";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import "./NavbarStyles.css";
 
 function Navbar() {
   return (
@@ -18,6 +20,9 @@ function Navbar() {
       <div className="nav-icons">
         <BiSearch className="icon" />
         <BsPerson className="icon" />
+      </div>
+      <div className="hamburger">
+        <HiOutlineMenuAlt4 className="icon" />
       </div>
     </div>
   );
