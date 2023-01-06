4f23efce9b9a876a366ab1b154d118fada82ab18
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-06 19:14:41 +0200

    add babground video

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./HeroStyles.css";
+import Video from "../../assets/maldivesVideo.mp4";
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <video autoPlay loop id="video">
+        <source src={Video} type="video/mp4" />
+      </video>
+    </div>
+  );
 }
 
 export default Hero;
